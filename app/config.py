68121934cf7ab83bc4942a269d6130cf7f092e59
Evgen4567 <aes1993rich@gmail.com>
2022-07-03 10:46:58 +0500

    to webhooks

--- app/config.py
+++ app/config.py
@@ -28,3 +28,11 @@ ADMINS_CHAT_ID = [int(x) for x in ADMINS_CHAT_ID]
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 URAALA_ARTIKUL_PATH = f"{ROOT_DIR}/conv_artikuls_uraala.csv"
+
+# webhook settings
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TELEGRAM_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
 from aiogram import executor
+from aiogram.utils.executor import start_webhook
 
-from bot import dp
-from config import logger
+from bot import dp, bot
+from config import logger, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_PORT, WEBAPP_HOST
+
+
+async def on_startup(dispatcher):
+    await bot.delete_webhook()
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 
 if __name__ == '__main__':
     logger.info('Start bot')
-    executor.start_polling(dp, skip_updates=True)
+    # executor.start_polling(dp, skip_updates=True)
+
+    # webserver settings
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
+
